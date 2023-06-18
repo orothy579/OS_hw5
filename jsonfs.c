798fc29bdd3676ca965907fc4d4c172cfcde0aeb
LCH579 <21900579@handong.edu>
2023-06-19 02:22:36 +0900

    working with touch

--- jsonfs.c
+++ jsonfs.c
@@ -181,12 +181,75 @@ static int fuse_example_readdir(const char *path, void *buf, fuse_fill_dir_t fil
     return 0;
 }
 
+static int fuse_example_write(const char *path, const char *buf, size_t size, off_t offset,
+                              struct fuse_file_info *fi) {
+    int inode = lookup_inode(path);
+    if (inode < 0) return -ENOENT;
+
+    fs_object *obj = &fs_objects[inode];
+    if (obj->data) {
+        // Make sure the file is large enough to write the data.
+        size_t new_size = offset + size;
+        if (new_size > strlen(obj->data)) {
+            obj->data = realloc(obj->data, new_size + 1);  // +1 for the null terminator
+            if (!obj->data) return -ENOMEM;
+            memset(obj->data + strlen(obj->data), 0, new_size - strlen(obj->data));
+        }
+
+        // Write the data.
+        memcpy(obj->data + offset, buf, size);
+    } else {
+        // No data yet, so allocate a new buffer.
+        obj->data = calloc(1, size + 1);  // +1 for the null terminator
+        if (!obj->data) return -ENOMEM;
+        memcpy(obj->data, buf, size);
+    }
+
+    return size;
+}
+
+static int fuse_example_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+    printf("fuse_example_create called with path: %s\n", path);
+    
+    if (lookup_inode(path) >= 0) return -EEXIST;
+
+    // Allocate a new fs_object.
+    num_fs_objects++;
+    fs_objects = realloc(fs_objects, num_fs_objects * sizeof(fs_object));
+    if (!fs_objects) return -ENOMEM;
+
+    // Initialize the new fs_object.
+    fs_object *new_obj = &fs_objects[num_fs_objects - 1];
+    memset(new_obj, 0, sizeof(fs_object));
+    new_obj->inode = num_fs_objects - 1;  // Assume that inodes are allocated sequentially.
+    new_obj->type = "reg";
+    new_obj->name = strdup(path + 1);  // Skip the leading slash
+    if (!new_obj->name) return -ENOMEM;
+    new_obj->data = NULL;  // Initially, the file has no data.
+    new_obj->entries = json_object_new_array();  // Since it's not a directory, entries is an empty array.
+
+    // Add the new file to the root directory.
+    fs_object *root_obj = &fs_objects[0];
+    struct json_object *entry_obj = json_object_new_object();
+    json_object_object_add(entry_obj, "name", json_object_new_string(new_obj->name));
+    json_object_object_add(entry_obj, "inode", json_object_new_int(new_obj->inode));
+    json_object_array_add(root_obj->entries, entry_obj);
+
+    // Open the new file.
+    fi->fh = new_obj->inode;
+
+    printf("fuse_example_create returning: %d\n", 0);
+    return 0;
+}
+
 
 static struct fuse_operations fuse_example_oper = {
     .getattr = fuse_example_getattr,
     .open = fuse_example_open,
     .read = fuse_example_read,
     .readdir = fuse_example_readdir,
+	.write = fuse_example_write,
+	.create = fuse_example_create,
 };
 
 int main(int argc, char *argv[]) {
